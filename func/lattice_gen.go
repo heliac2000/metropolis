9af80493c9b4fb29f3dd2e9038ba05c434acc929
Takahashi Mamoru <pitman0016@gmail.com>
2015-06-30 17:02:49 +0900

    Initial import.

---
+++ func/lattice_gen.go
@@ -0,0 +1,49 @@
+//
+// lattice_gen.go
+//
+
+package functions
+
+import . "../util"
+
+// Generate a lattice based on the unit cell
+//
+func LatticeGen(UnitCell, LatticeVectors [][]float64) ([][]float64, []int) {
+	// Number of lattice points in the unit cell
+	nlp := len(UnitCell)
+
+	// Generate the coordinates of the lattice from the unit cell
+	LatticeCoords := Create2DimArrayFloat(nlp, 2, (nlp+1)*Nrepeat*Nrepeat/4)
+
+	// horizontal/vertical coordinate
+	for k := 0; k < nlp; k++ {
+		LatticeCoords[k][0], LatticeCoords[k][1] = UnitCell[k][0], UnitCell[k][1]
+	}
+
+	// Specify which unit cell point the lattice point corresponds to
+	Character := make([]int, nlp, (nlp+1)*Nrepeat*Nrepeat/4)
+	for i := 0; i < nlp; i++ {
+		Character[i] = i + 1
+	}
+
+	avec, bvec := LatticeVectors[0], LatticeVectors[1]
+	UnitCellC := Copy2DimArrayFloat(LatticeCoords)
+	for k := 0; k < Nrepeat/2; k++ {
+		for j := 0; j < Nrepeat/2; j++ {
+			LatticeTemp := Copy2DimArrayFloat(UnitCellC)
+			for h := 0; h < nlp; h++ {
+				LatticeTemp[h][0] += float64(k)*avec[0] + float64(j)*bvec[0]
+				LatticeTemp[h][1] += float64(k)*avec[1] + float64(j)*bvec[1]
+			}
+			LatticeCoords = append(LatticeCoords, LatticeTemp...)
+			for i := 0; i < nlp; i++ {
+				Character = append(Character, i+1)
+			}
+		}
+	}
+
+	// Matrix of lattice coordinates
+	Out, Remv := MatrixTidy(LatticeCoords)
+
+	return Out, Remv
+}
---
+++ func/lattice_gen_test.go
@@ -0,0 +1,48 @@
+package functions_test
+
+import (
+	"math"
+	"testing"
+
+	. "../util"
+	. "./"
+)
+
+type testCasesLatticeGen struct {
+	UC, LV    [][]float64
+	Lattice   [][]float64
+	Character []int
+}
+
+func TestLatticeGen(t *testing.T) {
+	testCases := []testCasesLatticeGen{
+		{
+			UC: LoadFromCsvFile2DimFloat("./data/PrecursorUnitCell.csv", ' '),
+			LV: LoadFromCsvFile2DimFloat("./data/PrecursorUnitCellAxes.csv", ','),
+			//
+			// [R] write.table(Lattice, file="lattice.csv", sep=",", row.names=FALSE, col.names=FALSE)
+			Lattice:   LoadFromCsvFile2DimFloat("./data/lattice.csv", ','),
+			Character: []int{0, 0, 1},
+		},
+	}
+
+	for _, l := range testCases {
+		lat, char := LatticeGen(l.UC, l.LV)
+		if len(lat) != len(l.Lattice) {
+			t.Errorf("\ngot  %v, %v\nwant %v, %v", lat, char, l.Lattice, l.Character)
+			return
+		}
+		for i := 0; i < len(lat); i++ {
+			for j := 0; j < len(lat[0]); j++ {
+				if math.Abs(lat[i][j]-l.Lattice[i][j]) > 1.0E-06 {
+					t.Errorf("\ngot  [%d][%d] %v\nwant %v", i, j, lat[i][j], l.Lattice[i][j])
+					return
+				}
+			}
+		}
+	}
+}
+
+// Local Variables:
+// compile-command: (concat "go test -v " (file-name-nondirectory buffer-file-name))
+// End:
