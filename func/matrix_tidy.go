a122205a92bc4f4e472568468b3d60126b7e4399
Takahashi Mamoru <pitman0016@gmail.com>
2015-07-01 13:00:35 +0900

    条件判定を関数化。

--- func/matrix_tidy.go
+++ func/matrix_tidy.go
@@ -9,15 +9,17 @@ import "math"
 // Remove duplicated rows from the matrix X, or coordinates within
 // epsilon
 //
+func withinEpsilon(Zk, Zj []float64) bool {
+	return math.Sqrt(
+		(Zk[0]-Zj[0])*(Zk[0]-Zj[0])+(Zk[1]-Zj[1])*(Zk[1]-Zj[1])) < Epsilon
+}
+
 func MatrixTidy(Z [][]float64) ([][]float64, []int) {
 	remove := make([]int, len(Z))
 	for k := 0; k < len(Z)-1; k++ {
 		for j := k + 1; j < len(Z); j++ {
-			if remove[j] == 0 {
-				if math.Sqrt((Z[k][0]-Z[j][0])*(Z[k][0]-Z[j][0])+
-					(Z[k][1]-Z[j][1])*(Z[k][1]-Z[j][1])) < Epsilon {
-					remove[j] = 1
-				}
+			if remove[j] == 0 && withinEpsilon(Z[k], Z[j]) {
+				remove[j] = 1
 			}
 		}
 	}
