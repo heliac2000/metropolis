99c0b7f28363a773e0b28516cf88ae21468309d6
Takahashi Mamoru <pitman0016@gmail.com>
2015-07-16 11:13:31 +0900

    Initial import.

---
+++ util/which.go
@@ -0,0 +1,41 @@
+package util
+
+// R's `which' functions
+//
+
+// func WhichOverZero(hop int, ahop [][]int) []int {
+// 	l := len(ahop[hop])
+// 	hop_d := make([]int, 0, l)
+// 	for i := 0; i < l; i++ {
+// 		if ahop[hop][i] > 0 {
+// 			hop_d = append(hop_d, i)
+// 		}
+// 	}
+//
+// 	return hop_d
+// }
+
+func WhichIn(a, b []int) bool {
+
+	for _, v1 := range a {
+		for _, v2 := range b {
+			if v1 == v2 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func WhichNotIn(a, b []int) []int {
+	c := make([]int, 0, len(a))
+
+	for _, v1 := range a {
+		if !Member(v1, b) {
+			c = append(c, v1)
+		}
+	}
+
+	return c
+}
---
+++ util/which_test.go
@@ -0,0 +1,85 @@
+package util_test
+
+import (
+	"reflect"
+	"testing"
+)
+import . "./"
+
+// WhichOverZero
+//
+func TestWhichOverZero(t *testing.T) {
+	hop := [][]int{{0, 1, 2, 3, -1, 10, 15, -20}}
+	actual := WhichOverZero(0, hop)
+	expected := []int{1, 2, 3, 5, 6}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("\ngot  %v\nwant %v", actual, expected)
+		return
+	}
+}
+
+// WhichIn
+//
+type testCasesWhichIn struct {
+	arr_a, arr_b []int
+	expected     bool
+}
+
+func TestWhichIn(t *testing.T) {
+	testCases := []testCasesWhichIn{
+		{
+			arr_a:    []int{1, 2, 3},
+			arr_b:    []int{3, 4, 5, 6},
+			expected: true,
+		},
+		{
+			arr_a:    []int{1, 2, 3},
+			arr_b:    []int{4, 5, 6},
+			expected: false,
+		},
+	}
+
+	var actual bool
+	for _, tc := range testCases {
+		actual = WhichIn(tc.arr_a, tc.arr_b)
+		if actual != tc.expected {
+			t.Errorf("\ngot  %v\nwant %v", actual, tc.expected)
+			return
+		}
+	}
+}
+
+// WhichNotIn
+//
+type testCasesWhichNotIn struct {
+	arr_a, arr_b, expected []int
+}
+
+func TestWhichNotIn(t *testing.T) {
+	testCases := []testCasesWhichNotIn{
+		{
+			arr_a:    []int{1, 2, 3},
+			arr_b:    []int{3, 4, 5, 6},
+			expected: []int{1, 2},
+		},
+		{
+			arr_a:    []int{1, 2, 3},
+			arr_b:    []int{4, 5, 6},
+			expected: []int{1, 2, 3},
+		},
+	}
+
+	var actual []int
+	for _, tc := range testCases {
+		actual = WhichNotIn(tc.arr_a, tc.arr_b)
+		if !reflect.DeepEqual(actual, tc.expected) {
+			t.Errorf("\ngot  %v\nwant %v", actual, tc.expected)
+			return
+		}
+	}
+}
+
+// Local Variables:
+// compile-command: (concat "go test -v " (file-name-nondirectory buffer-file-name))
+// End:
