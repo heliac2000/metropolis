da526bd8e85dbcc8482d1540dbeab2b39911371e
Takahashi Mamoru <pitman0016@gmail.com>
2015-07-22 17:12:06 +0900

    Add charactersOrientations.

--- func/set_init_data.go
+++ func/set_init_data.go
@@ -7,29 +7,30 @@ package functions
 import . "../util"
 
 type InitData struct {
-	UnitCell       [][]float64
-	UnitCellCoords [][]float64
-	AdjCuml        [][][]int
-	Character      []int
-	ChUnique       []int
+	UnitCell               [][]float64
+	UnitCellCoords         [][]float64
+	AdjCuml                [][][]int
+	Character              []int
+	ChUnique               []int
+	CharactersOrientations [][]int
 }
 
 func SetInitData(ucFile, lvFile string) {
-	UnitCell := LoadFromCsvFile2Dim(ucFile, ' ')
+	unitCell := LoadFromCsvFile2Dim(ucFile, ' ')
 	LatticeVectors := LoadFromCsvFile2Dim(lvFile, ',')
 
-	Lattice, Character := LatticeGen(UnitCell, LatticeVectors)
+	Lattice, character := LatticeGen(unitCell, LatticeVectors)
 
-	for i := 0; i < len(Character); i++ {
-		if Character[i] >= 0 && Character[i] <= 2 {
-			Character[i] = 6 - Character[i]
+	for i := 0; i < len(character); i++ {
+		if character[i] >= 0 && character[i] <= 2 {
+			character[i] = 6 - character[i]
 		}
 	}
 
 	// Identify the unit cells by those which have character == 4
-	wh4 := make([]int, 0, len(Character))
-	for i := 0; i < len(Character); i++ {
-		if Character[i] == 3 {
+	wh4 := make([]int, 0, len(character))
+	for i := 0; i < len(character); i++ {
+		if character[i] == 3 {
 			wh4 = append(wh4, i)
 		}
 	}
@@ -39,9 +40,9 @@ func SetInitData(ucFile, lvFile string) {
 
 	// Number of unit cells
 	nUC := len(wh4)
-	UnitCellCoords := Create2DimArray(float64(0), nUC, 2).([][]float64)
+	unitCellCoords := Create2DimArray(float64(0), nUC, 2).([][]float64)
 	for k := 0; k < nUC; k++ {
-		copy(UnitCellCoords[k], Lattice[wh4[k]])
+		copy(unitCellCoords[k], Lattice[wh4[k]])
 	}
 
 	avec, bvec := LatticeVectors[0], LatticeVectors[1]
@@ -49,7 +50,7 @@ func SetInitData(ucFile, lvFile string) {
 	Adj := Create2DimArray(int(0), nUC, nUC).([][]int)
 	for j := 0; j < nUC; j++ {
 		for i := 0; i < 4; i++ {
-			copy(Moves[i], UnitCellCoords[j])
+			copy(Moves[i], unitCellCoords[j])
 		}
 		Moves[0][0] += avec[0]
 		Moves[1][0] -= avec[0]
@@ -58,7 +59,7 @@ func SetInitData(ucFile, lvFile string) {
 		Moves[3][0] -= bvec[0]
 		Moves[3][1] -= bvec[1]
 
-		_, surrj := GetKnnx(UnitCellCoords, Moves, 1)
+		_, surrj := GetKnnx(unitCellCoords, Moves, 1)
 		surr := make([]int, len(surrj))
 		for i := 0; i < len(surrj); i++ {
 			surr[i] = surrj[i][0]
@@ -79,20 +80,36 @@ func SetInitData(ucFile, lvFile string) {
 		AdjSEQ[k] = MatrixMultiply(AdjSEQ[k-1], Adj)
 	}
 
-	AdjCuml := make([][][]int, Npower)
-	AdjCuml[0] = Copy2DimArray(AdjSEQ[0]).([][]int)
+	adjCuml := make([][][]int, Npower)
+	adjCuml[0] = Copy2DimArray(AdjSEQ[0]).([][]int)
 	for k := 1; k < Npower; k++ {
-		AdjCuml[k] = Copy2DimArray(AdjSEQ[0]).([][]int)
+		adjCuml[k] = Copy2DimArray(AdjSEQ[0]).([][]int)
 		for j := 1; j <= k; j++ {
-			MatrixAdd(AdjCuml[k], AdjSEQ[j])
+			MatrixAdd(adjCuml[k], AdjSEQ[j])
+		}
+	}
+
+	// Generate all combinations of characters and corresponding
+	// orientations. First space is empty - it gets filled in
+	// Extension.Block
+	//
+	// charactersOrientations[][1] is index(0-base or 1-base)
+	chUnique := Unique(character)
+	charactersOrientations := Create2DimArray(int(0), len(chUnique)*3, 3).([][]int)
+	for k, cnt := 0, 0; k < len(chUnique); k++ {
+		for j := 0; j < 3; j++ {
+			charactersOrientations[cnt][1] = chUnique[k]
+			charactersOrientations[cnt][2] = int(unitCell[chUnique[k]][j+4])
+			cnt++
 		}
 	}
 
 	Inp = &InitData{
-		UnitCell:       UnitCell,
-		UnitCellCoords: UnitCellCoords,
-		AdjCuml:        AdjCuml,
-		Character:      Character,
-		ChUnique:       Unique(Character),
+		UnitCell:               unitCell,
+		UnitCellCoords:         unitCellCoords,
+		AdjCuml:                adjCuml,
+		Character:              character,
+		ChUnique:               chUnique,
+		CharactersOrientations: charactersOrientations,
 	}
 }
