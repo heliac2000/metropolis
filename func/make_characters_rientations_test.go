b0ab787eb47ee58793e6303bc6948c66601c5ba3
Takahashi Mamoru <pitman0016@gmail.com>
2015-07-23 13:55:43 +0900

    Update.

--- func/make_characters_rientations_test.go
+++ func/make_characters_rientations_test.go
@@ -19,11 +19,9 @@ func TestMakeCharactersRientations(t *testing.T) {
 
 	testCases := []testCasesMakeCharactersRientations{
 		{
-			// NOTICE: R is 1-base index, golang is 0-base.
-			//
 			// [R] makeCharactersOrientations(1,
 			//       t(array(c(22,33,44,55,66,77),3,2)),t(as.matrix(c(1,2,3,4,5,6,7,8,9,10,11,12))))
-			//     t1[,2] = t1[,2] - 1
+			//     t1[,2] = t1[,2] - 1 ## R is 1-base index, golang is 0-base.
 			//     write.table(t1, file="makeCharactersOrientations_01.dat", row.names=FALSE, col.names=FALSE, sep=",")
 			//
 			zcoords:  [][]float64{{22, 33, 44}, {55, 66, 77}},
