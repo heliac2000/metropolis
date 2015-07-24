ad06d635ba53ee0743483d420603ab5baac1173d
Takahashi Mamoru <pitman0016@gmail.com>
2015-07-24 17:12:28 +0900

    Initial import.

---
+++ func/extension_block.go
@@ -0,0 +1,47 @@
+//
+// extension_block.go
+//
+
+package functions
+
+import (
+	. "../util"
+)
+
+// Generate the extension of Island. Island is in form of (unit cell
+// labels, characters, labels)
+//
+func ExtensionBlock(xtest []int, zcoords [][]float64) ([][][]int, int) {
+	// Count the number of elements in the extension set
+	lx := 0
+	xtestAppend := make([][][]int, 0, len(Inp.AdjCuml[Npower-1]))
+
+	if len(xtest) == 1 && xtest[0] == 0 {
+		xadd := Copy2DimArray(Inp.CharactersOrientations).([][]int)
+		for i := 0; i < len(xadd); i++ {
+			xadd[i][0] = UCcenter
+		}
+		xtestAppend = append(xtestAppend, xadd)
+		lx = len(Inp.CharactersOrientations)
+	} else {
+		xsurr := Unique(SurrAdj(xtest, Inp.AdjCuml[Npower-1]))
+		// Is there a way to get rid of this loop?
+		for k := 0; k < len(xsurr); k++ {
+			// Make the CharacersOrientations based checking for overlap
+			// condition. Must make this faster
+			xadd := MakeCharactersRientations(zcoords, []int{xsurr[k]})
+			if l := len(xadd) * len(xadd[0]); l > 0 {
+				if l == 3 {
+					xadd = Transpose(xadd).([][]int)
+				}
+				lx += len(xadd)
+				for i := 0; i < len(xadd); i++ {
+					xadd[i][0] = xsurr[k]
+				}
+				xtestAppend = append(xtestAppend, xadd)
+			}
+		}
+	}
+
+	return xtestAppend, lx
+}
---
+++ func/extension_block_test.go
@@ -0,0 +1,49 @@
+package functions_test
+
+import (
+	"reflect"
+	"testing"
+
+	. "../util"
+	. "./"
+)
+
+type testCasesExtensionBlock struct {
+	xtest       []int
+	zcoords     [][]float64
+	xtestAppend [][][]int
+	lx          int
+}
+
+func TestMakeExtensionBlock(t *testing.T) {
+	SetInitData("./data/PrecursorUnitCell.csv", "./data/PrecursorUnitCellAxes.csv")
+
+	testCases := []testCasesExtensionBlock{
+		{
+			// [R] t1 = Extension.Block(list(c(10,20,30), c(5,3,4), c(6,7,8)),
+			//            array(c(1.0, 2.0, 3.0, 4.0, 5.0, 6.0), c(2, 3)))[[1]]
+			//     ## R is 1-base index, golang is 0-base.
+			//     for (i in 1:length(t1)){ t1[[i]][,2] = t1[[i]][,2] - 1 }
+			//     writeListData(t1, "ExtensionBlock_01.csv")
+			//
+			xtest:       []int{10, 20, 30},
+			zcoords:     [][]float64{{1.0, 3.0, 5.0}, {2.0, 4.0, 6.0}},
+			xtestAppend: LoadFromCsvFileList("./data/ExtensionBlock_01.csv"),
+			lx:          1176,
+		},
+	}
+
+	for _, tc := range testCases {
+		xtestAppend, lx := ExtensionBlock(tc.xtest, tc.zcoords)
+		if !reflect.DeepEqual(xtestAppend, tc.xtestAppend) || lx != tc.lx {
+			t.Errorf("\ngot  %v\nwant %v", lx, tc.lx)
+			return
+		}
+	}
+
+	return
+}
+
+// Local Variables:
+// compile-command: (concat "go test -v " (file-name-nondirectory buffer-file-name))
+// End:
