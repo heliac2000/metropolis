a5147bfa686faa5a84c7d7bf3308628c8d68b92f
Takahashi Mamoru <pitman0016@gmail.com>
2015-07-27 16:03:47 +0900

    Update.

--- func/canonical_gen.go
+++ func/canonical_gen.go
@@ -9,13 +9,12 @@ package functions
 // Generate a random canonical representation of random islands. For
 // now, make all sizes = 1
 //
-func CanonicalGen() ([][][]int, [][][]int, [][][]float64) {
+func CanonicalGen() ([][]int, [][]int, [][]float64) {
 	xout, cout, oout :=
-		make([][][]int, Nmolec), make([][][]int, Nmolec), make([][][]float64, Nmolec)
+		make([][]int, Nmolec), make([][]int, Nmolec), make([][]float64, Nmolec)
 
 	for k := 0; k < Nmolec; k++ {
-		isn1, isn2, isn3 := RandomIslandUnitCell(1)
-		xout[k], cout[k], oout[k] = [][]int{isn1}, [][]int{isn2}, [][]float64{isn3}
+		xout[k], cout[k], oout[k] = RandomIslandUnitCell(1)
 	}
 
 	return xout, cout, oout
