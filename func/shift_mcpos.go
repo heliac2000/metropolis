c00c37f26decf12525b77de624d5bbdfe1fa579a
Takahashi Mamoru <pitman0016@gmail.com>
2015-07-31 09:57:39 +0900

    Update.

--- func/shift_mcpos.go
+++ func/shift_mcpos.go
@@ -8,19 +8,21 @@ import . "../util"
 
 // Put COM of molecule at coordinate X (1 x 2 vector)
 //
-func ShiftMCpos(MoleculeCoords *MoleculeCoordinates, X []float64) [][]float64 {
-	MoleculeCoordsShift := Copy2DimArray(MoleculeCoords.All).([][]float64)
-	r := len(MoleculeCoords.All)
-	c1, c2 := make([]float64, r), make([]float64, r)
+func ShiftMCpos(moleculeCoords *MoleculeCoordinates, x []float64) [][]float64 {
+	shift := Copy2DimArray(moleculeCoords.All).([][]float64)
+	r := len(shift)
+
+	sumX, sumY := 0.0, 0.0
 	for i := 0; i < r; i++ {
-		c1[i], c2[i] = MoleculeCoordsShift[i][0], MoleculeCoordsShift[i][1]
+		sumX += shift[i][0]
+		sumY += shift[i][1]
 	}
 
-	y1, y2 := X[0]-Average(c1), X[1]-Average(c2)
+	y1, y2 := x[0]-sumX/float64(r), x[1]-sumY/float64(r)
 	for i := 0; i < r; i++ {
-		MoleculeCoordsShift[i][0] += y1
-		MoleculeCoordsShift[i][1] += y2
+		shift[i][0] += y1
+		shift[i][1] += y2
 	}
 
-	return MoleculeCoordsShift
+	return shift
 }
