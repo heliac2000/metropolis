643126ed9639430dc80a0b7063af2242ea475739
Takahashi Mamoru <pitman0016@gmail.com>
2015-08-04 13:47:25 +0900

    Initial import.

---
+++ func/island_symmetry_block.go
@@ -0,0 +1,49 @@
+//
+// island_symmetry_block.go
+//
+
+package functions
+
+import (
+	. "../util"
+)
+
+// Compute the symmetry of block CaB
+//
+func IslandSymmetryBlock(cab []int) int {
+	if cab[0] == 0 {
+		return 2
+	}
+
+	l := len(cab)
+	xtest := make([][]float64, 0, l)
+	for i := 0; i < l; i++ {
+		xtest = append(xtest, CopyVector(Inp.UnitCellCoords[cab[i]]).([]float64))
+	}
+
+	xtestr := make([][]float64, 0, l)
+	for _, v := range RotationBlock(cab) {
+		xtestr = append(xtestr, CopyVector(Inp.UnitCellCoords[v]).([]float64))
+	}
+
+	// if l == 1 {
+	// 	xtest, xtestr = Transpose(xtest).([][]float64), Transpose(xtestr).([][]float64)
+	// }
+
+	rmn, cnt := make([]int, len(xtestr)), 0
+	for k := 0; k < len(xtest); k++ {
+		for j := 0; j < len(xtestr); j++ {
+			if rmn[j] == 0 &&
+				xtest[k][0] == xtestr[j][0] && xtest[k][1] == xtestr[j][1] {
+				rmn[j], cnt = 1, cnt+1
+				break
+			}
+		}
+	}
+
+	if cnt == len(xtestr) || len(xtestr) == 1 {
+		return 2
+	}
+
+	return 1
+}
---
+++ func/island_symmetry_block_test.go
@@ -0,0 +1,47 @@
+package functions_test
+
+import (
+	"testing"
+
+	. "./"
+)
+
+type testCasesIslandSymmetry struct {
+	cab      []int
+	expected int
+}
+
+func TestIslandSymmetry(t *testing.T) {
+	SetInitData("./data/PrecursorUnitCell.csv", "./data/PrecursorUnitCellAxes.csv")
+
+	testCases := []testCasesIslandSymmetry{
+		// [R]
+		// IslandSymmetryBlock(list(c(1),c(1),c(1)))
+		// => 2
+		// IslandSymmetryBlock(list(c(313, 338, 415, 261, 337, 239, 282, 292, 253),c(1),c(1)))
+		// => 1
+		//
+		{
+			cab:      []int{1},
+			expected: 2,
+		},
+		{
+			cab:      []int{313, 338, 415, 261, 337, 239, 282, 292, 253},
+			expected: 1,
+		},
+	}
+
+	for _, tc := range testCases {
+		actual := IslandSymmetryBlock(tc.cab)
+		if actual != tc.expected {
+			t.Errorf("\ngot  %v\nwant %v", actual, tc.expected)
+			return
+		}
+	}
+
+	return
+}
+
+// Local Variables:
+// compile-command: (concat "go test -v " (file-name-nondirectory buffer-file-name))
+// End:
