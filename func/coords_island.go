7dd889a3d39b7f711c9d9874a4a13ae7dd0b652c
Takahashi Mamoru <pitman0016@gmail.com>
2015-08-04 10:22:37 +0900

    Update.

--- func/coords_island.go
+++ func/coords_island.go
@@ -17,17 +17,17 @@ import (
 // CoutO: Orientations(angle)
 func CoordsIsland(coutX, coutC []int, coutO []float64) [][]float64 {
 	l := len(coutX)
-	coords1 := Create2DimArray(float64(0), l, 3).([][]float64)
+	coords := Create2DimArray(float64(0), l, 3).([][]float64)
 	for i := 0; i < l; i++ {
-		coords1[i][0] = Inp.UnitCellCoords[coutX[i]][0] - (Inp.UnitCell2[3][1] - Inp.UnitCell2[coutC[i]][1])
-		coords1[i][1] = Inp.UnitCellCoords[coutX[i]][1] - (Inp.UnitCell2[3][1] - Inp.UnitCell2[coutC[i]][2])
+		coords[i][0] = Inp.UnitCellCoords[coutX[i]][0] - (Inp.UnitCell2[3][1] - Inp.UnitCell2[coutC[i]][1])
+		coords[i][1] = Inp.UnitCellCoords[coutX[i]][1] - (Inp.UnitCell2[3][1] - Inp.UnitCell2[coutC[i]][2])
 	}
 
-	IslandTemp := make([][]float64, 0, l*len(Inp.MoleculeCoordinates.All))
+	islandTemp := make([][]float64, 0, l*len(Inp.MoleculeCoordinates.All))
 	for j := 0; j < l; j++ {
-		M1 := ShiftMCpos(Inp.MoleculeCoordinates, coords1[j])
-		IslandTemp = append(IslandTemp, RotateZ(M1, coutO[j]*math.Pi/180.0)...)
+		m := ShiftMCpos(Inp.MoleculeCoordinates, coords[j])
+		islandTemp = append(islandTemp, RotateZ(m, coutO[j]*math.Pi/180.0)...)
 	}
 
-	return IslandTemp
+	return islandTemp
 }
