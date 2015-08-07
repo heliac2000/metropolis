8144088c008d3a717fde974eaab3059aa08e858c
Takahashi Mamoru <pitman0016@gmail.com>
2015-08-07 11:40:38 +0900

    Update.

--- func/matrix_tidy.go
+++ func/matrix_tidy.go
@@ -9,28 +9,30 @@ import "math"
 // Remove duplicated rows from the matrix X, or coordinates within
 // epsilon
 //
-func withinEpsilon(Zk, Zj []float64) bool {
+func withinEpsilon(zk, zj []float64) bool {
 	return math.Sqrt(
-		(Zk[0]-Zj[0])*(Zk[0]-Zj[0])+(Zk[1]-Zj[1])*(Zk[1]-Zj[1])) < Epsilon
+		(zk[0]-zj[0])*(zk[0]-zj[0])+(zk[1]-zj[1])*(zk[1]-zj[1])) < Epsilon
 }
 
-func MatrixTidy(Z [][]float64) ([][]float64, []int) {
-	remove := make([]int, len(Z))
-	for k := 0; k < len(Z)-1; k++ {
-		for j := k + 1; j < len(Z); j++ {
-			if remove[j] == 0 && withinEpsilon(Z[k], Z[j]) {
+func MatrixTidy(z [][]float64) ([][]float64, []int) {
+	l := len(z)
+	remove := make([]int, l)
+
+	for k := 0; k < l-1; k++ {
+		for j := k + 1; j < l; j++ {
+			if remove[j] == 0 && withinEpsilon(z[k], z[j]) {
 				remove[j] = 1
 			}
 		}
 	}
 
-	Z2 := make([][]float64, 0, len(Z))
-	Z2 = append(Z2, Z[0])
-	for j := 1; j < len(Z); j++ {
+	z2 := make([][]float64, 0, l)
+	z2 = append(z2, z[0])
+	for j := 1; j < l; j++ {
 		if remove[j] == 0 {
-			Z2 = append(Z2, Z[j])
+			z2 = append(z2, z[j])
 		}
 	}
 
-	return Z2, remove
+	return z2, remove
 }
--- func/matrix_tidy_test.go
+++ func/matrix_tidy_test.go
@@ -7,7 +7,7 @@ import (
 import . "./"
 
 type testCasesMatrixTidy struct {
-	Z, Z2   [][]float64
+	z, z2   [][]float64
 	removed []int
 }
 
@@ -16,15 +16,15 @@ func TestMatrixTidy(t *testing.T) {
 		{
 			// [R] MatrixTidy(t(array(c(1,2,5,7,1,2), c(2,3))))
 			//
-			Z:       [][]float64{{1, 2}, {5, 7}, {1, 2}},
-			Z2:      [][]float64{{1, 2}, {5, 7}},
+			z:       [][]float64{{1, 2}, {5, 7}, {1, 2}},
+			z2:      [][]float64{{1, 2}, {5, 7}},
 			removed: []int{0, 0, 1},
 		},
 		{
 			// [R] MatrixTidy(t(array(c(1,2,5,7,1,2,5,7.4,5,7.9), c(2,5))))
 			//
-			Z:       [][]float64{{1, 2}, {5, 7}, {1, 2}, {5, 7.4}, {5, 7.9}},
-			Z2:      [][]float64{{1, 2}, {5, 7}, {5, 7.9}},
+			z:       [][]float64{{1, 2}, {5, 7}, {1, 2}, {5, 7.4}, {5, 7.9}},
+			z2:      [][]float64{{1, 2}, {5, 7}, {5, 7.9}},
 			removed: []int{0, 0, 1, 1, 0},
 		},
 	}
@@ -32,14 +32,14 @@ func TestMatrixTidy(t *testing.T) {
 	var z2 [][]float64
 	var r []int
 	for _, z := range testCases {
-		z2, r = MatrixTidy(z.Z)
-		if !reflect.DeepEqual(z2, z.Z2) || !reflect.DeepEqual(r, z.removed) {
-			t.Errorf("\ngot  %v, %v\nwant %v, %v", z2, r, z.Z2, z.removed)
+		z2, r = MatrixTidy(z.z)
+		if !reflect.DeepEqual(z2, z.z2) || !reflect.DeepEqual(r, z.removed) {
+			t.Errorf("\ngot  %v, %v\nwant %v, %v", z2, r, z.z2, z.removed)
 			return
 		}
 	}
 }
 
 // Local Variables:
-// compile-command: (concat "go test -v " (file-name-nondirectory buffer-file-name))
+// compile-command: (concat "go test -gcflags='-B' -v " (file-name-nondirectory buffer-file-name))
 // End:
