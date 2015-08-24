3d2ac1eaa8fa4379c7d84747f89fe640d1762241
Takahashi Mamoru <pitman0016@gmail.com>
2015-08-24 16:17:47 +0900

    Update.

--- func/canonical_gen.go
+++ func/canonical_gen.go
@@ -12,27 +12,11 @@ package functions
 func CanonicalGen() ([][]int, [][]int, [][]float64) {
 	// Need to make this better - a truly uniform random initial
 	// condition please!
-	sizes := make([]int, Nmolec)
-	for k, sum := 0, 0; k < Nmolec; k++ {
-		if k == Nmolec-1 {
-			sizes[k] = Nmolec - sum
-		} else {
-			sizes[k] = Rnd.Intn(Nmolec - sum + 1)
-			sum += sizes[k]
-			if sum == Nmolec {
-				break
-			}
-		}
-	}
 
 	xout, cout, oout :=
 		make([][]int, Nmolec), make([][]int, Nmolec), make([][]float64, Nmolec)
 	for k := 0; k < Nmolec; k++ {
-		if sizes[k] > 0 {
-			xout[k], cout[k], oout[k] = RandomIslandUnitCell(sizes[k])
-		} else {
-			xout[k], cout[k], oout[k] = []int{0}, []int{0}, []float64{0}
-		}
+		xout[k], cout[k], oout[k] = RandomIslandUnitCell(1)
 	}
 
 	return xout, cout, oout
