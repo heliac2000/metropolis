474c7fcbd14fa8549717a78abd510ca2ec60d3be
Takahashi Mamoru <pitman0016@gmail.com>
2015-08-25 12:58:46 +0900

    Initial import.

---
+++ func/reaction_representation.go
@@ -0,0 +1,81 @@
+//
+// reaction_representation.go
+//
+
+package functions
+
+import (
+	"sort"
+
+	. "../util"
+)
+
+// Try out reaction representation of Ci --> Cj transformation
+// Reactant is Ci, Product is Cj
+//
+func ReactionRepresentation(prct, crct [][]int, orct [][]float64,
+	ppdt, cpdt [][]int, opdt [][]float64) ([][]int, [][]int, [][]float64, []int, []int, []int) {
+
+	cPrct, cPpdt := Copy2DimArray(prct).([][]int), Copy2DimArray(ppdt).([][]int)
+	cCrct, cCpdt := Copy2DimArray(crct).([][]int), Copy2DimArray(cpdt).([][]int)
+	cOrct, cOpdt := Copy2DimArray(orct).([][]float64), Copy2DimArray(opdt).([][]float64)
+	ptot := Create2DimArray(int(0), len(prct)+len(ppdt), len(prct[0])).([][]int)
+	ctot := Create2DimArray(int(0), len(crct)+len(cpdt), len(crct[0])).([][]int)
+	otot := Create2DimArray(float64(0), len(orct)+len(opdt), len(orct[0])).([][]float64)
+
+	copy(ptot, cPrct)
+	copy(ptot[len(cPrct):], cPpdt)
+	copy(ctot, cCrct)
+	copy(ctot[len(cCrct):], cCpdt)
+	copy(otot, cOrct)
+	copy(otot[len(cOrct):], cOpdt)
+
+	// Rapid check for isomorphism between islands
+
+	// Number of islands to consider
+	nI := len(ptot)
+	// Assign a label to each island according to its equivalence class
+	labels, done := make([]int, nI), make([]int, nI)
+
+	for k := 0; k < nI-1; k++ {
+		if done[k] == 0 {
+			done[k], labels[k] = 1, k
+			for j := k + 1; j < nI; j++ {
+				// [R code] if(length(Xk) != length(Xj)) dont = TRUE
+				// lenght(Xk) and length(Xj) are always 1.
+				if (ptot[k][0] == 0 && ptot[j][0] == 0) ||
+					IsomorphIslandsBlock(ptot[k], ctot[k], otot[k], ptot[j], ctot[j], otot[j]) {
+					labels[j], done[j] = k, 1
+				}
+			}
+		}
+	}
+
+	// Identifies the unique classes
+	labUnq := Unique(labels)
+	sort.Ints(labUnq)
+
+	l := len(labUnq)
+	pil := Create2DimArray(int(0), l, len(ptot[0])).([][]int)
+	cil := Create2DimArray(int(0), l, len(ctot[0])).([][]int)
+	oil := Create2DimArray(float64(0), l, len(otot[0])).([][]float64)
+	for i, k := range labUnq {
+		pil[i], cil[i], oil[i] = ptot[k], ctot[k], otot[k]
+	}
+
+	// lab1:Labels for reactant side, lab2:Labels for product side
+	lab1, lab2 := CopyVector(labels[:len(prct)]).([]int), CopyVector(labels[len(prct):]).([]int)
+	// Coefficients for reactant side, Coefficients for product side
+	coeffsRct, coeffsPdt := make([]int, 0, l), make([]int, 0, l)
+	for _, k := range labUnq {
+		coeffsRct = append(coeffsRct, CountItems(k, lab1))
+		coeffsPdt = append(coeffsPdt, CountItems(k, lab2))
+	}
+
+	diff := make([]int, l)
+	for i := 0; i < l; i++ {
+		diff[i] = coeffsPdt[i] - coeffsRct[i]
+	}
+
+	return pil, cil, oil, coeffsRct, coeffsPdt, diff
+}
---
+++ func/reaction_representation_test.go
@@ -0,0 +1,85 @@
+package functions_test
+
+import (
+	"reflect"
+	"testing"
+
+	. "./"
+)
+
+type testCasesReactionRepresentation struct {
+	pcanon, ccanon             [][]int
+	ocanon                     [][]float64
+	pcanon_out, ccanon_out     [][]int
+	ocanon_out                 [][]float64
+	pil, cil                   [][]int
+	oil                        [][]float64
+	coeffsRct, coeffsPdt, diff []int
+}
+
+func TestReactionRepresentation(t *testing.T) {
+	SetInitData("./data/PrecursorUnitCell.csv", "./data/UnitCell2.csv", "./data/PrecursorUnitCellAxes.csv")
+
+	// NOTICE: R is 1-base index, golang is 0-base.
+	//
+	testCases := []testCasesReactionRepresentation{
+		{
+			// [R]
+			// canon = Canonical.Order(Canonical.Gen())
+			// canon_out = Canonical.Order(ExtensionReductionBlock(canon)[[1]])
+			// =>
+			// canon = list(
+			//           list(313, 313, 313, 313, 313, 313, 313, 313, 313, 313, 0),
+			//           list(4, 4, 6, 6, 5, 5, 5, 6, 4, 5, 0),
+			//           list(90, 0, 60, 120, 30, 120, 60, 0, 90, 30, 0))
+			//
+			// canon_out = list(
+			//           list(313, 313, 313, 313, 313, 313, 313, 313, 313, 313, 0),
+			//           list(4, 4, 6, 6, 5, 5, 5, 6, 4, 5, 0),
+			//           list(90, 0, 60, 120, 30, 120, 60, 0, 90, 30, 0))
+			//
+			pcanon:     [][]int{{313}, {313}, {313}, {313}, {313}, {313}, {313}, {313}, {313}, {313}, {0}},
+			ccanon:     [][]int{{3}, {3}, {5}, {5}, {4}, {4}, {4}, {5}, {3}, {4}, {0}},
+			ocanon:     [][]float64{{90}, {0}, {60}, {120}, {30}, {120}, {60}, {0}, {90}, {30}, {0}},
+			pcanon_out: [][]int{{313}, {313}, {313}, {313}, {313}, {313}, {313}, {313}, {313}, {313}, {0}},
+			ccanon_out: [][]int{{3}, {3}, {5}, {5}, {4}, {4}, {4}, {5}, {3}, {4}, {0}},
+			ocanon_out: [][]float64{{90}, {0}, {60}, {120}, {30}, {120}, {60}, {0}, {90}, {30}, {0}},
+			pil:        [][]int{{313}, {313}, {313}, {313}, {313}, {313}, {313}, {313}, {0}},
+			cil:        [][]int{{3}, {3}, {5}, {5}, {4}, {4}, {4}, {5}, {0}},
+			oil:        [][]float64{{90}, {0}, {60}, {120}, {30}, {120}, {60}, {0}, {0}},
+			coeffsRct:  []int{2, 1, 1, 1, 2, 1, 1, 1, 1},
+			coeffsPdt:  []int{2, 1, 1, 1, 2, 1, 1, 1, 1},
+			diff:       []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		},
+	}
+
+	for _, tc := range testCases {
+		pil, cil, oil, coeffsRct, coeffsPdt, diff := ReactionRepresentation(
+			tc.pcanon, tc.ccanon, tc.ocanon, tc.pcanon_out, tc.ccanon_out, tc.ocanon_out)
+		if !reflect.DeepEqual(pil, tc.pil) {
+			t.Errorf("\n[PIL] got  %v\nwant %v", pil, tc.pil)
+			return
+		} else if !reflect.DeepEqual(cil, tc.cil) {
+			t.Errorf("\n[CIL] got  %v\nwant %v", cil, tc.cil)
+			return
+		} else if !reflect.DeepEqual(oil, tc.oil) {
+			t.Errorf("\n[OIL] got  %v\nwant %v", oil, tc.oil)
+			return
+		} else if !reflect.DeepEqual(coeffsRct, tc.coeffsRct) {
+			t.Errorf("\n[CoeffsRct] got  %v\nwant %v", coeffsRct, tc.coeffsRct)
+			return
+		} else if !reflect.DeepEqual(coeffsPdt, tc.coeffsPdt) {
+			t.Errorf("\n[CoeffsPdt] got  %v\nwant %v", coeffsPdt, tc.coeffsPdt)
+			return
+		} else if !reflect.DeepEqual(diff, tc.diff) {
+			t.Errorf("\n[Diff] got  %v\nwant %v", diff, tc.diff)
+			return
+		}
+	}
+
+	return
+}
+
+// Local Variables:
+// compile-command: (concat "go test -gcflags='-B' -v " (file-name-nondirectory buffer-file-name))
+// End:
