92ee0120a82ab22603ba2a3313f1a98fe6217cfb
Takahashi Mamoru <pitman0016@gmail.com>
2015-08-25 14:43:31 +0900

    Initial import.

---
+++ func/reaction_class_id.go
@@ -0,0 +1,66 @@
+//
+// reaction_class_id.go
+//
+
+package functions
+
+import (
+	"reflect"
+
+	_ "../util"
+)
+
+// Next part: Calculation transistion probability based on reaction class
+// Key:
+const (
+	REACT_UNKNOWN int = iota - 1
+	REACT_CLASS1
+	REACT_CLASS2
+	REACT_CLASS3
+	REACT_CLASS4
+	REACT_CLASS5
+	REACT_CLASS6
+	REACT_CLASS7
+	REACT_CLASS8
+	REACT_CLASS9
+)
+
+var classKey = [][]int{
+	{-1, -1, +1, +1},
+	{-1, -1, +2},
+	{-1, -1, +1},
+	{-2, +1, +1},
+	{-2, +1},
+	{-1, +1, +1},
+	{-1, +2},
+	{-1, +1},
+	{},
+}
+
+// Identify reaction class according to scheme above
+//
+//
+func ReactionClassID(diff []int) int {
+	// Fetch the reaction difference vector
+	//_, _, _, _, _, diff := ReactionRepresentation(prct, crct, orct, ppdt, cpdt, opdt)
+
+	// Later, use the full data in dIJ to calculate the probabilities
+	dijp := make([]int, 0, len(diff))
+	for _, v := range diff {
+		if v != 0 {
+			dijp = append(dijp, v)
+		}
+	}
+
+	if len(dijp) > 4 || len(dijp) == 1 {
+		return REACT_UNKNOWN
+	}
+
+	for i, v := range classKey {
+		if reflect.DeepEqual(dijp, v) {
+			return i
+		}
+	}
+
+	return REACT_UNKNOWN
+}
---
+++ func/reaction_class_id_test.go
@@ -0,0 +1,47 @@
+package functions_test
+
+import (
+	"testing"
+
+	. "./"
+)
+
+type testCasesReactionClassId struct {
+	diff     []int
+	expected int
+}
+
+func TestReactionClassID(t *testing.T) {
+	SetInitData("./data/PrecursorUnitCell.csv", "./data/UnitCell2.csv", "./data/PrecursorUnitCellAxes.csv")
+
+	// NOTICE: R is 1-base index, golang is 0-base.
+	//
+	testCases := []testCasesReactionClassId{
+		{
+			diff:     []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			expected: 8, // REACT_CLASS9
+		},
+		{
+			diff:     []int{0, -1, 0, 0, 2, 0, 0, 0, 0},
+			expected: 6, // REACT_CLASS7
+		},
+		{
+			diff:     []int{0, 3, 0, 0, 0, 0, 0, 0, 0},
+			expected: -1, // REACT_UNKNOWN
+		},
+	}
+
+	for _, tc := range testCases {
+		actual := ReactionClassID(tc.diff)
+		if actual != tc.expected {
+			t.Errorf("\ngot  %v\nwant %v", actual, tc.expected)
+			return
+		}
+	}
+
+	return
+}
+
+// Local Variables:
+// compile-command: (concat "go test -gcflags='-B' -v " (file-name-nondirectory buffer-file-name))
+// End:
