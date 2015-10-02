04cf77325d3ee217fa2508f6735492379dcb1b22
Takahashi Mamoru <pitman0016@gmail.com>
2015-10-02 16:04:10 +0900

    Update.

--- func/energy_pair_test.go
+++ func/energy_pair_test.go
@@ -1,14 +1,17 @@
 package functions_test
 
 import (
+	"math"
 	"testing"
 
 	. "./"
 )
 
-type testEnegyPair struct {
+type testCasesEnegyPair struct {
 	k1, k2, ch1, ch2 int
 	o1, o2           float64
+	intType          string
+	eint             float64
 }
 
 func TestEnergyPair(t *testing.T) {
@@ -16,14 +19,28 @@ func TestEnergyPair(t *testing.T) {
 		"./data/PrecursorUnitCell.csv", "./data/UnitCell2.csv", "./data/PrecursorUnitCellAxes.csv",
 		"./data/kernelregS_Rep_log.json", "./data/kernelregS_Att.json", "./data/svm_model.json")
 
-	testCases := []testEnegyPair{
-		{313, 363, 6, 6, 0, 0},
-		{31, 63, 1, 1, 2, 1},
+	// [R]
+	//
+	// format(EnergyPair(31, 63, 1, 1, 2, 1), digits=22, trim=T)
+	//
+	testCases := []testCasesEnegyPair{
+		{
+			k1: 313, k2: 363, ch1: 6, ch2: 6, o1: 0, o2: 0,
+			intType: "repulsive", eint: 0.9265127673648503314752,
+		},
+		{
+			k1: 31, k2: 63, ch1: 1, ch2: 1, o1: 2, o2: 1,
+			intType: "attractive", eint: -0.05134541334315906313535,
+		},
 	}
 
 	for _, tc := range testCases {
 		intType, eint := EnergyPair(tc.k1, tc.k2, tc.ch1, tc.ch2, tc.o1, tc.o2)
-		t.Logf("\nType   = %v\nEnergy = %v\n", intType, eint)
+		if tc.intType != intType ||
+			math.Abs(tc.eint-eint) > 10E-12 {
+			t.Errorf("\nExpected: Type   = %v, Energy = %v\n  Actual: Type   = %v, Energy = %v\n",
+				tc.intType, tc.eint, intType, eint)
+		}
 	}
 }
 
