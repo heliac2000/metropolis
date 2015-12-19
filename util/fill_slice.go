d325446714f68add595f1c2168c46be2f1a879bf
Takahashi Mamoru <pitman0016@gmail.com>
2015-12-19 21:43:53 +0900

    []int に限定。

--- util/fill_slice.go
+++ util/fill_slice.go
@@ -1,21 +1,25 @@
 package util
 
-import "reflect"
-
 // Fill slice with a value
 //
-func FillSlice(slc, val interface{}) {
-	sv := reflect.ValueOf(slc)
-	if sv.Kind() != reflect.Slice {
-		panic("fill: slc expected slice")
-	}
-
-	vv := reflect.ValueOf(val)
-	if vv.Type() != sv.Type().Elem() {
-		panic("fill: val type != slc element type")
-	}
-
-	for i := 0; i < sv.Len(); i++ {
-		sv.Index(i).Set(vv)
+func FillSlice(slc []int, val int) {
+	for i := 0; i < len(slc); i++ {
+		slc[i] = val
 	}
 }
+
+// func FillSlice(slc, val interface{}) {
+// 	sv := reflect.ValueOf(slc)
+// 	if sv.Kind() != reflect.Slice {
+// 		panic("fill: slc expected slice")
+// 	}
+//
+// 	vv := reflect.ValueOf(val)
+// 	if vv.Type() != sv.Type().Elem() {
+// 		panic("fill: val type != slc element type")
+// 	}
+//
+// 	for i := 0; i < sv.Len(); i++ {
+// 		sv.Index(i).Set(vv)
+// 	}
+// }
