a2a5a94f7a0d24e412bc374e44bcf50933515ed4
Takahashi Mamoru <pitman0016@gmail.com>
2016-01-15 12:25:54 +0900

    Update.

--- func/broken_island_test.go
+++ func/broken_island_test.go
@@ -12,9 +12,7 @@ type testCasesBrokenIsland struct {
 }
 
 func TestBrokenIslandUnitCell(t *testing.T) {
-	SetInitData(
-		"./data/PrecursorUnitCell.csv", "./data/UnitCell2.csv", "./data/PrecursorUnitCellAxes.csv",
-		"./data/kernelregS_Rep_log.json", "./data/kernelregS_Att.json", "./data/svm_model.json")
+	SetInitData()
 
 	testCases := []testCasesBrokenIsland{
 		{
