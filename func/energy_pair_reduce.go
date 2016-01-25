32c715a53af9e5cdc675fb3bec8c5574fe78652b
Takahashi Mamoru <pitman0016@gmail.com>
2016-01-25 16:56:47 +0900

    Initial import.

---
+++ func/energy_pair_reduce.go
@@ -0,0 +1,53 @@
+//
+// energy_pair_reduce.go
+//
+
+package functions
+
+import "math"
+
+// Compute the interaction energy of a molecule at unit cell k1, in
+// character c1, in orientation o1, and a molecule at unit cel k2,
+// character c2, in orientation o2. Orientations in degrees. Uses the
+// reduced representation based on PCA and four-way classification
+// (see notes pg. 264 - 267)
+//
+func EnergyPairReduce(k1, k2, c1, c2 int, o1, o2 float64) float64 {
+	cmTest, outside := CoulombIntMatrix(k1, k2, c1, c2, o1, o2)
+
+	// outside = false means that the molecules are within the cut-off range
+	if outside {
+		return 0.0
+	}
+
+	cmVec := CoulombVectorise(cmTest)
+	cmVecRed := CMvecReduce(cmVec, Npcs)
+
+	// Zero SVM
+	typ := "zero"
+	opClass := SvmModelOp.Predict(cmVecRed)
+	// Attractive SVM
+	if opClass == "nonzero" {
+		switch a := SvmModelNzp.Predict(cmVecRed); a {
+		case "attractive":
+			typ = a
+		case "repulsive":
+			r := SvmModelUsp.Predict(cmVecRed)
+			if r == "repulsive" || r == "unstable" {
+				typ = r
+			}
+		}
+	}
+
+	eAdd := 0.0
+	switch typ {
+	case "unstable":
+		eAdd = Eunstable
+	case "attractive":
+		eAdd = KernelRegsAtt.Predict(cmVecRed)
+	case "repulsive":
+		eAdd = math.Exp(KernelRegsRepLog.Predict(cmVecRed))
+	}
+
+	return eAdd
+}
---
+++ func/energy_pair_reduce_test.go
@@ -0,0 +1,44 @@
+package functions_test
+
+import (
+	"math"
+	"testing"
+
+	. "./"
+)
+
+type testCasesEnergyPairReduce struct {
+	k1, k2, c1, c2 int
+	o1, o2         float64
+	expected       float64
+}
+
+func TestEnergyPairReduce(t *testing.T) {
+	SetInitData()
+
+	// NOTICE: R is 1-base index, golang is 0-base.
+	//   ch1 and ch2 are characters, 1-base in R but 0-base in Golang
+	//
+	// [R]
+	// write.table(format(CoulombIntMatrix(31, 63, 1, 1, 2, 1)[[1]], digits=22, trim=T),
+	//             file="CoulombIntMatrix_01.csv", sep=",", row.names=FALSE, col.names=FALSE, quote=F)
+	//
+	testCases := []testCasesEnergyPairReduce{
+		{
+			k1: 1, k2: 2, c1: 2, c2: 3, o1: 5, o2: 6,
+			expected: 10.0,
+		},
+	}
+
+	for _, tc := range testCases {
+		actual := EnergyPairReduce(tc.k1, tc.k2, tc.c1, tc.c2, tc.o1, tc.o2)
+		if math.Abs(actual-tc.expected) > 1.0E-10 {
+			t.Errorf("\ngot %.22f\nwant %.22f", actual, tc.expected)
+			return
+		}
+	}
+}
+
+// Local Variables:
+// compile-command: (concat "go test -v " (file-name-nondirectory buffer-file-name))
+// End:
