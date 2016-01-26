03c74db1f24be0fd7ea31267d2769406d26da91e
Takahashi Mamoru <pitman0016@gmail.com>
2016-01-26 13:07:53 +0900

    Initial import.

---
+++ func/energy_island_pca.go
@@ -0,0 +1,36 @@
+//
+// energy_island_pca.go
+//
+
+package functions
+
+import "math"
+
+// Calculate energy of an island (Island[[1]] = positions, Island[[2]]
+// = characters, Island[[2]] = orientations). Only use this if PCA_REP
+// = TRUE
+//
+func EnergyIslandPCA(pos, chr []int, ori []float64) float64 {
+	// Adsorption energy
+	ene := 0.0
+	for k := 0; k < len(pos); k++ {
+		// Incase new UnitCell2 file format is used
+		for _, u := range Inp.UnitCell2 {
+			// u[0]: color, u[3]: angle, u[4]: energy
+			if int(u[0]) == Inp.Character[chr[k]]+1 && math.Abs(u[3]-ori[k]) < Detect {
+				ene += u[4]
+			}
+		}
+	}
+
+	eneInt := 0.0
+	if len(pos) > 1 {
+		for k := 0; k < len(pos)-1; k++ { // Interaction energy
+			for j := k + 1; j < len(pos); j++ {
+				eneInt += EnergyPairReduce(pos[k], pos[j], chr[k], chr[j], ori[k], ori[j])
+			}
+		}
+	}
+
+	return ene + eneInt
+}
---
+++ func/energy_island_pca_test.go
@@ -0,0 +1,61 @@
+package functions_test
+
+import (
+	"testing"
+
+	. "./"
+)
+
+type testCasesEnegyIslandPCA struct {
+	pos, chr []int
+	ori      []float64
+	eInt     float64
+}
+
+func TestEnergyIslandPCA(t *testing.T) {
+	SetInitData()
+
+	// NOTICE: R is 1-base index, golang is 0-base.
+	//   chr is characters, 1-base in R but 0-base in Golang
+	//
+	// [R]
+	// c <- Canonical.Order(Canonical.Gen())
+	// island <- list()
+	// island[[1]] = c[[1]][[1]] ## 1227 # position
+	// island[[2]] = c[[2]][[1]] ## 6    # character
+	// island[[3]] = c[[3]][[1]] ## 120  # orientation
+	// format(EnergyIslandPCA(island), digit=22)
+	// [1] "-2.037550000000123873178"
+	//
+	testCases := []testCasesEnegyIslandPCA{
+		{
+			pos:  []int{1227},
+			chr:  []int{5},
+			ori:  []float64{120},
+			eInt: -2.037550000000123873178,
+		},
+		{
+			pos:  []int{1227, 1227},
+			chr:  []int{5, 1},
+			ori:  []float64{120, 60},
+			eInt: 5.935699999999769715942,
+		},
+		{
+			pos:  []int{1227, 1227, 1227},
+			chr:  []int{5, 1, 8},
+			ori:  []float64{120, 60, 120},
+			eInt: 23.91498999999964780727,
+		},
+	}
+
+	for _, tc := range testCases {
+		eInt := EnergyIslandPCA(tc.pos, tc.chr, tc.ori)
+		if tc.eInt != eInt {
+			t.Errorf("\nExpected Energy = %.22f\n  Actual Energy = %.22f\n", tc.eInt, eInt)
+		}
+	}
+}
+
+// Local Variables:
+// compile-command: (concat "go test -v " (file-name-nondirectory buffer-file-name))
+// End:
