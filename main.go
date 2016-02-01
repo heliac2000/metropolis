d38dee41f35b9854b36169b6d0cf7db47ebb5fd7
Takahashi Mamoru <pitman0016@gmail.com>
2016-02-01 14:44:33 +0900

    Update.

--- main.go
+++ main.go
@@ -20,9 +20,7 @@ func main() {
 	Temp = temp
 
 	// Load initial data
-	SetInitData(
-		"./data/PrecursorUnitCell.csv", "./data/UnitCell2.csv", "./data/PrecursorUnitCellAxes.csv",
-		"./data/kernelregS_Rep_log.json", "./data/kernelregS_Att.json", "./data/svm_model.json")
+	SetInitData()
 
 	// Start
 	log.Printf("N = %d\n", N)
